df2f201ff4d264f67bc4be14736a2bc5bb8df5a3
Stemmy287 <steem287@gmail.com>
2022-11-30 11:27:50 +0300

    lesson-7

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,32 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+import {exec} from "child_process";
+
+
+export type SortActionType = {
+    type: 'sort'
+    payload: 'up' | 'down'
+}
+
+export type CheckActionType = {
+    type: 'check'
+    payload: 18
+}
+
+export type ActionType = SortActionType | CheckActionType
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
             // need to fix
-            return state
+            return action.payload === 'up'
+                ? [...state].sort((a, b) => a.name > b.name ? 1 : -1)
+                : [...state].sort((a, b) => a.name < b.name ? 1 : -1)
         }
         case 'check': {
             // need to fix
-            return state
+            return state.filter(el => el.age >= 18)
         }
-        default: return state
+        default:
+            return state
     }
 }
